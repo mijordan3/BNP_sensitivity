c0dd8554e4d5313fd4257d1c7924c88d669e0974
Runjing <runjing_liu@berkeley.edu>
2018-11-21 11:34:18 -0800

    separate library for expected num cluster computations and tests

--- GMM_clustering/test_cluster_quantities_lib.py
+++ GMM_clustering/test_cluster_quantities_lib.py
@@ -3,13 +3,15 @@
 import autograd.numpy as np
 import autograd.scipy as sp
 
-import modeling_lib
+import cluster_quantities_lib
 import unittest
 
 np.random.seed(24524)
 
 class TestClusteringSamples(unittest.TestCase):
-    def test_clustering_samples(self):
+    def test_sampling_clusters_from_uniform(self):
+        # check the sampling of _get_clusters_from_ez_and_unif_samples
+
         # cluster belonging probabilities
         n_obs = 5
         n_clusters = 3
@@ -22,7 +24,8 @@ class TestClusteringSamples(unittest.TestCase):
         unif_samples = np.random.random((n_obs, n_samples))
 
         # get cluster belongings from uniform samples
-        z_ind_samples = modeling_lib.get_clusters_from_ez_and_unif_samples(\
+        z_ind_samples = \
+            cluster_quantities_lib._get_clusters_from_ez_and_unif_samples(\
                                         e_z_cumsum, unif_samples)
 
         # sample
@@ -31,32 +34,33 @@ class TestClusteringSamples(unittest.TestCase):
             e_z_sampled[:, i] = (z_ind_samples == i).mean(axis = 1)
 
         tol = 3 * np.sqrt(e_z * (1 - e_z) / n_samples)
-        print('e_z diff', np.max(np.abs(e_z_sampled - e_z)))
-        assert np.all(np.abs(e_z_sampled - e_z) < tol)
+
+        assert np.all(np.abs(e_z_sampled - e_z) < tol), \
+                    'diff is {}'.format(np.max(np.abs(e_z_sampled - e_z)))
 
     def test_get_e_num_clusters_from_ez(self):
+        # check that get_e_num_clusters_from_ez, which computes
+        # the expected number of clusters via MC matches the
+        # analytic expectation
+
         n_obs = 5
         n_clusters = 3
         e_z = np.random.random((n_obs, n_clusters))
         e_z = e_z / np.sum(e_z, axis = 1, keepdims = True)
 
         e_num_clusters_sampled, var_num_clusters_sampled = \
-            modeling_lib.get_e_num_large_clusters_from_ez(e_z,
+            cluster_quantities_lib.get_e_num_large_clusters_from_ez(e_z,
                                                     n_samples = 10000,
                                                     unif_samples = None,
-                                                    threshold = 0.0)
+                                                    threshold = 0)
 
-        e_num_clusters_analytic = modeling_lib.get_e_number_clusters_from_ez(e_z)
+        e_num_clusters_analytic = \
+            cluster_quantities_lib.get_e_num_clusters_from_ez(e_z)
 
-        print('e_num_clusters diff',
-                np.abs(e_num_clusters_analytic - e_num_clusters_sampled))
         assert np.abs(e_num_clusters_analytic - e_num_clusters_sampled) < \
-                    np.sqrt(var_num_clusters_sampled) * 3
-
-
-
-
-
+                    np.sqrt(var_num_clusters_sampled) * 3, \
+                    'diff: {}'.format(np.abs(e_num_clusters_analytic - \
+                                                e_num_clusters_sampled))
 
 if __name__ == '__main__':
     unittest.main()
