85b934f038afd23f94d0e37dc171e84839cc6cd4
Runjing <runjing_liu@berkeley.edu>
2018-12-05 00:13:06 -0800

    draft 3 of talk

--- writing/NIPS_2018_contributed_talk/Rscripts/plot_sens_results.R
+++ writing/NIPS_2018_contributed_talk/Rscripts/plot_sens_results.R
@@ -1,18 +1,30 @@
 plot_parametric_sensitivity <- function(
-    results_df, alpha_0, xlabel=TeX('$\\alpha$')) {
+    results_df, alpha_0, xlabel=TeX('$\\alpha$'), filter=FALSE) {
 
   results_df %>%
     gather(method, e_num_clusters, -alpha)
 
   results_df_long <- results_df %>%
     gather(method, e_num_clusters, -alpha)
-
-  plot <-
-    ggplot(results_df_long) +
-    geom_point(aes(x = alpha, y = e_num_clusters, color = method)) +
-    geom_line(aes(x = alpha, y = e_num_clusters, color = method)) +
-    xlab(xlabel) + ylab('E[# clusters]') +
-    theme(legend.position = c(0.75, 0.2))
+  
+  if(filter){
+    results_df_long <- results_df_long %>% filter(method == 'refitted')
+    plot <-
+      ggplot(results_df_long) +
+      geom_point(aes(x = alpha, y = e_num_clusters, color = method), color = '#00BFC4') +
+      geom_line(aes(x = alpha, y = e_num_clusters, color = method), color = '#00BFC4') +
+      xlab(xlabel) + ylab('E[# clusters]') +
+      theme(legend.position = c(0.75, 0.2))
+    
+  }
+  else{
+    plot <-
+      ggplot(results_df_long) +
+      geom_point(aes(x = alpha, y = e_num_clusters, color = method)) +
+      geom_line(aes(x = alpha, y = e_num_clusters, color = method)) +
+      xlab(xlabel) + ylab('E[# clusters]') +
+      theme(legend.position = c(0.75, 0.2))
+  }
 
   if(alpha_0 > 0){
     # if we actually want a vertical line
