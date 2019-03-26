9488ef6ac30c744581ad16bf39e1d8d941c52096
Runjing Liu <runjing_liu@arwen.Berkeley.EDU>
2019-03-26 12:59:02 -0700

    ran alpha perturbations

--- structure/vb_lib/scripts/get_alpha_perturbation_fit.sh
+++ structure/vb_lib/scripts/get_alpha_perturbation_fit.sh
@@ -9,7 +9,10 @@ nloci=50
 npop=4
 alpha=${alpha_vec[$SLURM_ARRAY_TASK_ID]}
 
+echo $alpha
+
 /usr/bin/env python3 ./get_structure_fit.py \
+  --alpha ${alpha} \
   --data_file ../data/simulated_structure_data__nobs${nobs}_nloci${nloci}_npop${npop} \
   --outfolder /scratch/users/genomic_times_series_bnp/structure_fits/ \
   --out_filename structure_fit_nobs${nobs}_nloci${nloci}_npop${npop}_alpha${alpha}
