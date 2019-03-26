f88e50d9d230401fa69a54565a9dc5c941ace667
Runjing Liu <runjing_liu@arwen.Berkeley.EDU>
2019-03-26 15:56:50 -0700

    fit takes long time ...

--- structure/vb_lib/scripts/get_one_structure_fit.sh
+++ structure/vb_lib/scripts/get_one_structure_fit.sh
@@ -12,8 +12,9 @@ alpha=5.0
 
 /usr/bin/env python3 ./get_structure_fit.py \
   --seed ${seed} \
+  --alpha $alpha \
   --data_file ../data/simulated_structure_data_nobs${nobs}_nloci${nloci}_npop${npop}.npz \
-  --outfolder ../fits/\
+  --outfolder /scratch/users/genomic_times_series_bnp/structure_fits/ \
   --out_filename testing
   # --outfolder /scratch/users/genomic_times_series_bnp/structure_fits/ \
   # --out_filename structure_fit_nobs${nobs}_nloci${nloci}_npop${npop}_alpha${alpha}
