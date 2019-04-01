ddbecc587fc5c99bbd7921666a469640f1a86a69
Runjing Liu <runjing_liu@arwen.Berkeley.EDU>
2019-04-01 16:25:41 -0700

    reran on small data to check

--- structure/vb_lib/scripts/get_alpha_perturbation_fit.sh
+++ structure/vb_lib/scripts/get_alpha_perturbation_fit.sh
@@ -4,8 +4,8 @@ source activate genomic_time_series_py35
 
 alpha_vec=(1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0)
 
-nobs=100
-nloci=2000
+nobs=50
+nloci=40
 npop=4
 alpha=${alpha_vec[$SLURM_ARRAY_TASK_ID]}
 
@@ -13,6 +13,7 @@ echo $alpha
 
 /usr/bin/env python3 ./get_structure_fit.py \
   --alpha ${alpha} \
+  --save_hessian True \
   --data_file ../data/simulated_structure_data_nobs${nobs}_nloci${nloci}_npop${npop}.npz \
   --outfolder /scratch/users/genomic_times_series_bnp/structure_fits/ \
   --out_filename structure_fit_nobs${nobs}_nloci${nloci}_npop${npop}_alpha${alpha}
