56821c8335768e6272b05dad13446a3ba34cf2d4
Ryan Giordano <rgiordan@gmail.com>
2019-06-05 13:51:08 -0700

    installable setup

---
+++ GMM_clustering/setup.py
@@ -0,0 +1,59 @@
+from os import path
+import pip
+import re
+from setuptools import setup, find_packages
+import sys
+
+here = path.abspath(path.dirname(__file__))
+
+print(here)
+
+pip_version_match = re.search(r'^[0-9]*', pip.__version__)
+if pip_version_match:
+    if int(pip_version_match.group(0)) < 19:
+        sys.exit('Install requires pip version 19 or greater.  ' +
+                 'Run pip install --upgrade pip.')
+else:
+    sys.exit('There was an error getting the pip version number.')
+
+# Parse requirements.txt, ignoring any commented-out or git lines.
+with open(path.join(here, 'requirements.txt')) as requirements_file:
+    requirements_lines = requirements_file.read().splitlines()
+
+requirements = [line for line in requirements_lines
+                if not (line.startswith('#')) ]
+
+# requirements = [line for line in requirements_lines
+#                 if not (line.startswith('#') or line.startswith('git'))]
+#
+# git_requirements = [line for line in requirements_lines
+#                      if line.startswith('git')]
+#
+# # git repos also need to be listed in the requirements.
+# for git_req in git_requirements:
+#     print('---------------\n', git_req, requirements)
+#     loc = git_requirements[0].find('egg=') + 4
+#     requirements += [ git_requirements[0][loc:] ]
+
+print(requirements)
+
+#exit(0)
+
+setup(
+    name='bnpgmm-runjingdev',
+    version='0.1',
+    description='Development version of GMM clustering scripts with BNP.',
+    author='Runjing Liu, Ryan Giordano',
+    author_email='runjing_liu@berkeley.edu',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ],
+)
