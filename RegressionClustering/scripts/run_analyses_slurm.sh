df22012127145a532d5fdde475ce3351341e6752
Ryan Giordano - SCF <rgiordan@gmail.com>
2019-06-13 21:17:39 -0700

    put header in sh file

--- RegressionClustering/scripts/run_analyses_slurm.sh
+++ RegressionClustering/scripts/run_analyses_slurm.sh
@@ -8,14 +8,15 @@ FIT_DIR="/accounts/grad/rgiordano/Documents/git_repos/BNP_sensitivity/Regression
 #GENES=7000
 GENES=700
 
-#INFLATE=0.0
-INFLATE=1.0
+INFLATE=0.0
+#INFLATE=1.0
 
 for REFIT_FILENAME in $(ls $FIT_DIR/*genes${GENES}*_inflate${INFLATE}_*refit.npz); do
     echo; echo; echo ------------------------------
     echo Running for $REFIT_FILENAME
     SCRIPT_FILENAME=$(mktemp)".sh"
     echo $SCRIPT_FILENAME
-    echo ./analyze_refit.py --fit_directory $FIT_DIR --refit_filename $REFIT_FILENAME > $SCRIPT_FILENAME
+    echo \#!/bin/bash >> $SCRIPT_FILENAME
+    echo ./analyze_refit.py --fit_directory $FIT_DIR --refit_filename $REFIT_FILENAME >> $SCRIPT_FILENAME
     sbatch $SCRIPT_FILENAME
 done
