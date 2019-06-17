461d7dc1688ae1f94360b6ad38b41dbc655015d9
Ryan Giordano <rgiordan@gmail.com>
2019-06-17 16:27:30 -0700

    functional results

--- writing/Oxford_2019_BNP_poster/R_graphs/genomics/functional_results.R
+++ writing/Oxford_2019_BNP_poster/R_graphs/genomics/functional_results.R
@@ -1,28 +1,26 @@
 
-results <- filter(gene_data$results, !functional)
+results <- filter(gene_data$results, functional)
 
 MakePlot <- function(use_preditive, use_inflate, use_alpha_increase, use_threshold=0) {
   ggplot(filter(results,
                 predictive==use_predictive,
                 inflate==use_inflate,
-                alpha_increase==use_alpha_increase,
                 threshold == use_threshold)) +
-    geom_point(aes(x=alpha1, y=e_num1, color="Refitting")) +
-    geom_point(aes(x=alpha1, y=e_num_pred, color="Approximation")) +
-    geom_line(aes(x=alpha1, y=e_num1, color="Refitting")) +
-    geom_line(aes(x=alpha1, y=e_num_pred, color="Approximation")) +
-    geom_vline(aes(xintercept=alpha0)) +
+    geom_point(aes(x=epsilon, y=e_num1, color="Refitting")) +
+    geom_point(aes(x=epsilon, y=e_num_pred, color="Approximation")) +
+    geom_line(aes(x=epsilon, y=e_num1, color="Refitting")) +
+    geom_line(aes(x=epsilon, y=e_num_pred, color="Approximation")) +
+    geom_vline(aes(xintercept=0.0)) +
     theme(legend.title = element_blank()) +
     ggtitle(sprintf("%s",
                     ifelse(use_inflate, "Noise added", "No noise added"))) +
-    xlab(TeX("$\\alpha$")) + ylab("Expected number of clusters")
+    xlab(TeX("$\\delta$")) + ylab("Expected number of clusters")
 }
 
 legend <- get_legend(MakePlot(use_predictive, FALSE, use_alpha_increase))
 nolegend <- theme(legend.position="None")
 
 use_predictive <- TRUE
-use_alpha_increase <- TRUE
 grid.arrange(
   MakePlot(use_predictive, FALSE, use_alpha_increase) + nolegend,
   MakePlot(use_predictive, TRUE, use_alpha_increase) + nolegend,
