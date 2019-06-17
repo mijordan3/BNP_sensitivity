a87b08bb42a4da7478062ca22170cdefc3773281
Ryan Giordano - SCF <rgiordan@gmail.com>
2019-06-17 12:49:17 -0700

    cluster changes

--- RegressionClustering/scripts/slurm_scripts/slurm_refits.sh
+++ RegressionClustering/scripts/slurm_scripts/slurm_refits.sh
@@ -5,7 +5,8 @@
 FIT_DIR="/accounts/grad/rgiordano/Documents/git_repos/BNP_sensitivity/RegressionClustering/fits"
 
 #for FIT_SCRIPT in $(ls ./*genes700_inflate?.0*.sh); do
-for FIT_SCRIPT in $(ls ./*genes7000_inflate?.0*.sh); do
+#for FIT_SCRIPT in $(ls ./*genes7000_inflate?.0*.sh); do
+for FIT_SCRIPT in $(ls ./*functionalpert_*genes700*_inflate?.0*.sh); do
     echo; echo; echo ------------------------------
     echo Running $FIT_SCRIPT
     sbatch $FIT_SCRIPT
