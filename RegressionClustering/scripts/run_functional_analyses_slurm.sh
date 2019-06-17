6db5ea959b3dc0a83c22233f0016e74cb2802ea8
Ryan Giordano - SCF <rgiordan@gmail.com>
2019-06-17 12:50:47 -0700

    run functional analsysi

--- RegressionClustering/scripts/run_functional_analyses_slurm.sh
+++ RegressionClustering/scripts/run_functional_analyses_slurm.sh
@@ -2,14 +2,8 @@
 
 # Run a number of initial fits on which to base our results.
 
-#FIT_DIR="/accounts/grad/rgiordano/Documents/git_repos/BNP_sensitivity/RegressionClustering/fits/"
-FIT_DIR="/home/rgiordan/Documents/git_repos/BNP_sensitivity/RegressionClustering/fits/cluster/"
-
-#GENES=7000
-#GENES=700
-
-#INFLATE=0.0
-#INFLATE=1.0
+FIT_DIR="/accounts/grad/rgiordano/Documents/git_repos/BNP_sensitivity/RegressionClustering/fits/"
+#FIT_DIR="/home/rgiordan/Documents/git_repos/BNP_sensitivity/RegressionClustering/fits/cluster/"
 
 for GENES in 700 7000; do
 for INFLATE in 0.0 1.0; do
@@ -20,7 +14,7 @@ for REFIT_FILENAME in $(ls $FIT_DIR/*genes${GENES}_*inflate${INFLATE}_*functiona
     echo $SCRIPT_FILENAME
     echo \#!/bin/bash >> $SCRIPT_FILENAME
     echo ./analyze_refit.py --fit_directory $FIT_DIR --refit_filename $REFIT_FILENAME >> $SCRIPT_FILENAME
-    #sbatch $SCRIPT_FILENAME
+    sbatch $SCRIPT_FILENAME
 done
 done
 done
