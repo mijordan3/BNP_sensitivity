e0b2a44b374fba87210ff1c0fbe19a486722724a
Ryan Giordano <rgiordan@gmail.com>
2019-06-19 10:59:22 -0700

    clean functional knitr

--- writing/Oxford_2019_BNP_poster/R_graphs/plot_sens_results.R
+++ writing/Oxford_2019_BNP_poster/R_graphs/plot_sens_results.R
@@ -1,3 +1,9 @@
+set_font_sizes <- theme(plot.title=element_text(size=20, face="bold"),
+                        axis.text=element_text(size=12),
+                        axis.title=element_text(size=18),
+                        legend.text=element_text(size=15))
+
+
 plot_parametric_sensitivity <- function(
     results_df, alpha_0, xlabel=TeX('$\\alpha$')) {
 
@@ -6,7 +12,8 @@ plot_parametric_sensitivity <- function(
     geom_point(aes(x = alpha, y = e_num_clusters, color = method)) +
     geom_line(aes(x = alpha, y = e_num_clusters, color = method)) +
     xlab(xlabel) + ylab('E[# clusters]') +
-    theme(legend.position = c(0.75, 0.2))
+    theme(legend.position = c(0.75, 0.2)) +
+    set_font_sizes
 
   if(alpha_0 > 0){
     # if we actually want a vertical line
@@ -16,8 +23,12 @@ plot_parametric_sensitivity <- function(
   return(plot)
 }
 
-
-set_font_sizes <- theme(plot.title=element_text(size=20, face="bold"),
-                        axis.text=element_text(size=12),
-                        axis.title=element_text(size=18),
-                        legend.text=element_text(size=15))
+plot_prior_perturbation <- function(prior_pert_df) {
+    plot <- ggplot(prior_pert_df) +
+        geom_line(aes(x = nu_k, y = p, color = which_prior)) +
+        theme(legend.title=element_blank()) +
+        xlab(TeX("$\\nu_k$")) + ylab(TeX("$p(\\nu_k)$")) +
+        scale_color_manual(values=c("red", "blue")) +
+        set_font_sizes
+    return(plot)
+}
