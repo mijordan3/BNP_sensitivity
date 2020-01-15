c67166806fd1a52365f67b95f308af114e9f5b4e
runjing_liu <runjing_liu@berkeley.edu>
2020-01-15 08:41:59 -0800

    revisiting preconditioners

--- structure/vb_lib/preconditioner_lib.py
+++ structure/vb_lib/preconditioner_lib.py
@@ -34,6 +34,31 @@ def get_log_beta_covariance(alpha, beta):
     return np.array([[I11 * alpha**2, I12 * alpha * beta], \
                      [I12 * alpha * beta, I22 * beta**2]])
 
+def get_mfvb_cov(vb_params_dict, vb_params_paragami,
+                    use_logitnormal_sticks,
+                    return_info = False):
+    # compute preconditioner from MFVB covariances
+
+    # get (constrained parameters):
+    vb_params = vb_params_paragami.flatten(vb_params_dict, free = False)
+
+    if use_logitnormal_sticks:
+        # TODO:
+        raise NotImplementedError()
+
+    block_mfvb_cov = ()
+    for i in range(len(vb_params) // 2):
+        # get covariance
+        cov = get_log_beta_covariance(vb_params[2 * i], vb_params[2 * i + 1])
+
+        if return_info:
+            cov = np.linalg.inv(cov)
+            
+        block_mfvb_cov = block_mfvb_cov + (cov, )
+
+    return sparse.block_diag(block_mfvb_cov)
+
+
 def get_mfvb_cov_preconditioner(vb_params_dict, vb_params_paragami,
                                 use_logitnormal_sticks):
     # compute preconditioner from MFVB covariances
@@ -64,39 +89,39 @@ def get_mfvb_cov_preconditioner(vb_params_dict, vb_params_paragami,
     return sparse.block_diag(block_mfvb_cov),\
                 sparse.block_diag(block_mfvb_info)
 
-
-class SystemSolverFromHVP:
-    def __init__(self, hvp, opt0, hyper0, cg_opts = {}):
-        self.hvp = hvp
-        self.opt0 = opt0
-        self.hyper0 = hyper0
-
-        self.hess_dim = len(opt0)
-
-        self.hvp_at_opt = osp.sparse.linalg.LinearOperator(\
-                                        shape = (self.hess_dim, self.hess_dim), \
-                                        matvec = lambda v : hvp(opt0, hyper0, v))
-        self.cg_opts = cg_opts
-
-    def solve(self, v):
-        assert len(v.shape) <= 2
-
-        if len(v.shape) == 1:
-            # if v is just a vector, make it (hess_dim x 1)
-            v = np.array([v]).T
-
-        n_vec = v.shape[1]
-        assert v.shape[0] == self.hess_dim
-
-        out = np.zeros((self.hess_dim, n_vec))
-
-        for i in range(n_vec):
-            cg_out = osp.sparse.linalg.cg(self.hvp_at_opt, v[:, i], **self.cg_opts)
-            out[:, i] = cg_out[0]
-
-            if not cg_out[1] == 0:
-                warning_message = 'conjugate-gradient tolerance not  ' + \
-                                'acheived after {} iterations'.format(cg_out[1])
-                warnings.warn(warning_message)
-
-        return out
+#
+# class SystemSolverFromHVP:
+#     def __init__(self, hvp, opt0, hyper0, cg_opts = {}):
+#         self.hvp = hvp
+#         self.opt0 = opt0
+#         self.hyper0 = hyper0
+#
+#         self.hess_dim = len(opt0)
+#
+#         self.hvp_at_opt = osp.sparse.linalg.LinearOperator(\
+#                                         shape = (self.hess_dim, self.hess_dim), \
+#                                         matvec = lambda v : hvp(opt0, hyper0, v))
+#         self.cg_opts = cg_opts
+#
+#     def solve(self, v):
+#         assert len(v.shape) <= 2
+#
+#         if len(v.shape) == 1:
+#             # if v is just a vector, make it (hess_dim x 1)
+#             v = np.array([v]).T
+#
+#         n_vec = v.shape[1]
+#         assert v.shape[0] == self.hess_dim
+#
+#         out = np.zeros((self.hess_dim, n_vec))
+#
+#         for i in range(n_vec):
+#             cg_out = osp.sparse.linalg.cg(self.hvp_at_opt, v[:, i], **self.cg_opts)
+#             out[:, i] = cg_out[0]
+#
+#             if not cg_out[1] == 0:
+#                 warning_message = 'conjugate-gradient tolerance not  ' + \
+#                                 'acheived after {} iterations'.format(cg_out[1])
+#                 warnings.warn(warning_message)
+#
+#         return out
