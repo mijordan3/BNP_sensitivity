135d57fead3932b773728c92ca6ccdf0138eb454
runjing_liu <runjing_liu@berkeley.edu>
2020-09-15 14:58:42 -0700

    new sensitivity class works

--- BNP_modeling/bnpmodeling_runjingdev/sensitivity_lib.py
+++ BNP_modeling/bnpmodeling_runjingdev/sensitivity_lib.py
@@ -25,7 +25,7 @@ class HyperparameterSensitivityLinearApproximation(object):
 
         # define derivatives
         self.dobj_dhyper = jax.jit(jax.jacobian(self.hyper_par_objective_fun, 1))
-        self.dobj_dhyper_dinput = jax.jit(jax.jacobian(self.hyper_par_objective_fun), 0)
+        self.dobj_dhyper_dinput = jax.jit(jax.jacobian(self.dobj_dhyper), 0)
 
         self.hess_fun = jax.jit(jax.hessian(self.objective_fun, argnums = 0))
 
@@ -42,16 +42,18 @@ class HyperparameterSensitivityLinearApproximation(object):
 
         # get dinput_dhyper
         t0 = time.time()
-        self.dinput_dhyper = self._solve_system()
-        print('Linear system time: {0:.3g}sec'.format(time.time() - t0))
+        _ = self._solve_system()
+        print('\nLinear system compile time: {0:.3g}sec'.format(time.time() - t0))
 
-        # get dinput_dhyper
         t0 = time.time()
         self.dinput_dhyper = self._solve_system()
         print('Linear system time: {0:.3g}sec'.format(time.time() - t0))
 
     def _compute_objective_derivatives(self):
-        cross_hess = self.dobj_dhyper_dinput(self.opt_par_value, self.hyper_par_value0)
+        cross_hess = self.dobj_dhyper_dinput(self.opt_par_value, \
+                                            self.hyper_par_value0)
+        cross_hess = cross_hess.transpose((1, 0))
+
         hessian = self.hess_fun(self.opt_par_value, self.hyper_par_value0)
 
         return cross_hess, hessian
