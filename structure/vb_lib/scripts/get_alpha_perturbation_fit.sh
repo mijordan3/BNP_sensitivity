bbcd5a0918648f4d76152ef289ef12d61e8e33aa
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2020-09-28 14:30:12 -0700

    alpha perturbation runs

--- structure/vb_lib/scripts/get_alpha_perturbation_fit.sh
+++ structure/vb_lib/scripts/get_alpha_perturbation_fit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source activate genomic_time_series_py35
+source activate bnp_sensitivity_jax
 
 alpha_vec=(1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0)
 
@@ -27,4 +27,4 @@ scratch_folder='/scratch/users/genomic_times_series_bnp/structure'
   --outfolder ${scratch_folder}/fits/fits_20200928/ \
   --out_filename structure_fit_nobs${nobs}_nloci${nloci}_npop${npop}_alpha${alpha} \
   --warm_start True \
-  --init_fit ${scratch_folder}/fits/structure_fit_nobs${nobs}_nloci${nloci}_npop${npop}_alpha3.5.npz
+  --init_fit ${scratch_folder}/fits/fits_20200928/structure_fit_nobs${nobs}_nloci${nloci}_npop${npop}_alpha3.5.npz
