9415baa85f1356cdade23fba3a265f559780c482
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2020-10-22 17:22:58 -0700

    function for loading in fast structure model fits

---
+++ structure/vb_lib/fast_structure_lib.py
@@ -0,0 +1,56 @@
+# utils for loading fast-structure model fits 
+
+import jax.numpy as np
+import numpy as onp
+
+from vb_lib.structure_model_lib import get_vb_params_paragami_object
+
+def load_fs_file_into_numpy(filename): 
+    handle = open(filename)
+    out = onp.array([line.strip().split() for line in handle]).astype('float')
+    handle.close()
+    return out
+
+def load_fs_means_vars(filename): 
+    
+    # individual admixtures
+    ind_admix_mean = load_fs_file_into_numpy(filename + '.meanQ')
+    ind_admix_var = load_fs_file_into_numpy(filename + '.varQ')
+    
+    # population frequencies
+    pop_freq_mean = load_fs_file_into_numpy(filename + '.meanP')
+    pop_freq_var = load_fs_file_into_numpy(filename + '.varP')
+    
+    return ind_admix_mean, ind_admix_var, pop_freq_mean, pop_freq_var
+
+def load_fs_to_vb_params(filename): 
+    
+    # load fitted means
+    _, ind_admix_var, _, pop_freq_var = \
+        load_fs_means_vars(filename)
+    
+    n_obs = ind_admix_var.shape[0]
+    n_loci = pop_freq_var.shape[0]
+    k_approx = ind_admix_var.shape[1]
+    assert pop_freq_var.shape[1] == (k_approx * 2)
+    
+    # set up vb parameters dictionary 
+    vb_params_dict, vb_params_paragami = \
+        get_vb_params_paragami_object(n_obs, n_loci, k_approx,
+                                        use_logitnormal_sticks = False)
+    
+    # make the vb params dict onp for the moment
+    # will change back later 
+    vb_params_dict['pop_freq_beta_params'] = onp.array(vb_params_dict['pop_freq_beta_params'])
+    vb_params_dict['ind_mix_stick_beta_params'] = onp.array(vb_params_dict['ind_mix_stick_beta_params'])
+    
+    # set beta parameters for pop frequency
+    vb_params_dict['pop_freq_beta_params'][:, :, 0] = pop_freq_var[:, 0:k_approx]
+    vb_params_dict['pop_freq_beta_params'][:, :, 1] = pop_freq_var[:, k_approx:]
+    
+    # set beta parameters for individual stick breaking
+    betas = onp.flip(onp.cumsum(onp.flip(ind_admix_var, 1), 1), 1)
+    vb_params_dict['ind_mix_stick_beta_params'][:, :, 0] = ind_admix_var[:, 0:(k_approx-1)]
+    vb_params_dict['ind_mix_stick_beta_params'][:, :, 1] = betas[:, 1:]
+    
+    return vb_params_dict, vb_params_paragami
---
+++ structure/vb_lib/plotting_utils.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+
+from bnpmodeling_runjingdev import modeling_lib, cluster_quantities_lib
+
+def plot_admixture(admixture, title):
+    # copied over form distruct.py file in faststructure
+    # adapted for python 3
+    
+    N,K = admixture.shape
+    colors = [colorsys.hsv_to_rgb(h,0.9,0.7) for h in np.linspace(0,1,K+1)[:-1]]
+    text_color = 'k'
+    bg_color = 'w'
+    fontsize = 12
+
+    figure = plt.figure(figsize=(5,3))
+
+    xmin = 0.13
+    ymin = 0.2
+    height = 0.6
+    width = 0.74
+    indiv_width = width/N
+    subplot = figure.add_axes([xmin,ymin,width,height])
+    [spine.set_linewidth(0.001) for spine in subplot.spines.values()]
+
+    for k in range(K):
+        if k:
+            bottoms = admixture[:,:k].sum(1)
+        else:
+            bottoms = np.zeros((N,),dtype=float)
+
+        lefts = np.arange(N)*indiv_width
+        subplot.bar(lefts, admixture[:,k], width=indiv_width, bottom=bottoms, facecolor=colors[k], edgecolor=colors[k], linewidth=0.4)
+
+        subplot.axis([0, N*indiv_width, 0, 1])
+        subplot.tick_params(axis='both', top=False, right=False, left=False, bottom=False)
+        xtick_labels = tuple(map(str,['']*N))
+        subplot.set_xticklabels(xtick_labels)
+        ytick_labels = tuple(map(str,['']*K))
+        subplot.set_yticklabels(ytick_labels)
+
+    position = subplot.get_position()
+    title_position = (0.5, 0.9)
+    figure.text(title_position[0], title_position[1], title, fontsize=fontsize, \
+        color='k', horizontalalignment='center', verticalalignment='center')
+
+
+def get_vb_expectations(vb_params_dict): 
+    
+    use_logitnormal_sticks = 'ind_mix_stick_propn_mean' in vb_params_dict.keys()
+    
+    if use_logitnormal_sticks: 
+        e_ind_admix = cluster_quantities_lib.get_e_cluster_probabilities(
+                            vb_params_dict['ind_mix_stick_propn_mean'], 
+                            vb_params_dict['ind_mix_stick_propn_info'],
+                            gh_loc, gh_weights)
+
+    else: 
+        ind_mix_stick_beta_params = vb_params_dict['ind_mix_stick_beta_params']
+        e_stick_lengths = \
+                modeling_lib.get_e_beta(ind_mix_stick_beta_params)
+        e_ind_admix = cluster_quantities_lib.get_mixture_weights_from_stick_break_propns(e_stick_lengths)
+
+    e_pop_freq = modeling_lib.get_e_beta(vb_params_dict['pop_freq_beta_params'])
+    
+    return e_ind_admix, e_pop_freq
