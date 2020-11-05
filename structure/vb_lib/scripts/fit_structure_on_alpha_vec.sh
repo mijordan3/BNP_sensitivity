9cfa3171b15db076554ff1ece02d6c5e1a8637ad
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2020-11-05 15:31:51 -0800

    run on hgdp for a range of alphas

--- structure/vb_lib/scripts/fit_structure_on_alpha_vec.sh
+++ structure/vb_lib/scripts/fit_structure_on_alpha_vec.sh
@@ -8,15 +8,13 @@ echo $alpha
 
 seed=453
 
-nobs=20
-nloci=50
-npop=4
-
+data_dir=/accounts/grad/runjing_liu/BNP/fastStructure/hgdp_data/huang2011_plink_files/
 out_folder=../fits/fits_20201105/
 
 python get_structure_fit.py \
   --seed ${seed} \
   --alpha ${alpha} \
-  --data_file ../simulated_data/simulated_structure_data_nobs${nobs}_nloci${nloci}_npop${npop}.npz \
+  --data_file ${data_dir}phased_HGDP+India+Africa_2810SNPs-regions1to36.npz \
   --out_folder ${out_folder} \
-  --out_filename structure_fit_nobs${nobs}_nloci${nloci}_npop${npop}_alpha${alpha}
+  --out_filename huang2011_fit_alpha${alpha} \
+  --k_approx 30
