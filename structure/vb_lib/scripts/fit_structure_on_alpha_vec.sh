2716e4a68e8c6629211b2d7b3a2ac4e614a8c099
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2020-11-06 14:54:36 -0800

    run hgdp on range of alpha

--- structure/vb_lib/scripts/fit_structure_on_alpha_vec.sh
+++ structure/vb_lib/scripts/fit_structure_on_alpha_vec.sh
@@ -10,15 +10,16 @@ echo $alpha
 
 seed=453
 
-data_dir=/accounts/grad/runjing_liu/BNP/fastStructure/test/
+data_file=/accounts/grad/runjing_liu/BNP/fastStructure/hgdp_data/huang2011_plink_files/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz
+out_filename=huang2011_fits_alpha
 out_folder=../fits/fits_20201106/
 
 python get_structure_fit.py \
   --seed ${seed} \
   --alpha ${alpha} \
-  --data_file ${data_dir}testdata.npz \
+  --data_file ${data_file} \
   --out_folder ${out_folder} \
-  --out_filename testdata_fit_alpha${alpha} \
+  --out_filename ${out_filename}${alpha} \
   --warm_start True \
-  --init_fit ${out_folder}testdata_fit_alpha6.0.npz \
-  --k_approx 20
+  --init_fit ${out_folder}${out_filename}6.0.npz \
+  --k_approx 30
--- structure/vb_lib/scripts/get_structure_fit.sh
+++ structure/vb_lib/scripts/get_structure_fit.sh
@@ -10,7 +10,7 @@ seed=453
 # out_filename=testdata_fits_alpha${alpha}
 
 data_file=/accounts/grad/runjing_liu/BNP/fastStructure/hgdp_data/huang2011_plink_files/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz 
-out_filename=huang2011_fits_alpha${alpha}
+out_filename=huang2011_fits_alpha
 
 out_folder=../fits/fits_20201106/
 
@@ -19,5 +19,5 @@ python get_structure_fit.py \
   --alpha ${alpha} \
   --data_file ${data_file} \
   --out_folder ${out_folder} \
-  --out_filename ${out_filename} \
+  --out_filename ${out_filename}${alpha} \
   --k_approx 30
