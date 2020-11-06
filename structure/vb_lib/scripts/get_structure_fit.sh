7a3e75880879e7238d7e7795030e805614dadf11
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2020-11-06 12:07:14 -0800

    now run on hgdp data

--- structure/vb_lib/scripts/get_structure_fit.sh
+++ structure/vb_lib/scripts/get_structure_fit.sh
@@ -6,13 +6,18 @@ alpha=6.0
 
 seed=453
 
-data_dir=/accounts/grad/runjing_liu/BNP/fastStructure/test/
+# data_file=/accounts/grad/runjing_liu/BNP/fastStructure/test/testdata.npz
+# out_filename=testdata_fits_alpha${alpha}
+
+data_file=/accounts/grad/runjing_liu/BNP/fastStructure/hgdp_data/huang2011_plink_files/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz 
+out_filename=huang2011_fits_alpha${alpha}
+
 out_folder=../fits/fits_20201106/
 
 python get_structure_fit.py \
   --seed ${seed} \
   --alpha ${alpha} \
-  --data_file ${data_dir}testdata.npz \
+  --data_file ${data_file} \
   --out_folder ${out_folder} \
-  --out_filename testdata_fit_alpha${alpha} \
-  --k_approx 20
+  --out_filename ${out_filename} \
+  --k_approx 30
