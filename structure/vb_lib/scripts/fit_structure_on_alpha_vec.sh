ae64dc78e0b5007bb241a6668838cd6863e06a33
Runjing Liu <runjing_liu@legolas.Berkeley.EDU>
2020-11-12 10:25:28 -0800

    parametric sensitivty works with subsampled hgdp data

--- structure/vb_lib/scripts/fit_structure_on_alpha_vec.sh
+++ structure/vb_lib/scripts/fit_structure_on_alpha_vec.sh
@@ -3,23 +3,28 @@
 source activate bnp_sensitivity_jax
 
 alpha_vec=(1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 \
-	   6.5 7.0 7.5 8.0 8.5 9.0 9.5 10.0 10.5 11.0)
+            6.5 7.0 7.5 8.0 8.5 9.0 9.5 10.0 10.5 11.0)
 
 alpha=${alpha_vec[$SLURM_ARRAY_TASK_ID]}
 echo $alpha 
 
 seed=453
 
-data_file=/accounts/grad/runjing_liu/BNP/fastStructure/hgdp_data/huang2011_plink_files/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz
-out_filename=huang2011_fits_alpha
-out_folder=../fits/fits_20201109/
+# data_file=/accounts/grad/runjing_liu/BNP/fastStructure/hgdp_data/huang2011_plink_files/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz
+# out_filename=huang2011_fit
+
+
+data_file=../data/huang2011_subsampled.npz
+out_filename=huang2011_sub_fit
+
+out_folder=../fits/fits_20201111/
 
 python get_structure_fit.py \
   --seed ${seed} \
   --alpha ${alpha} \
   --data_file ${data_file} \
   --out_folder ${out_folder} \
-  --out_filename ${out_filename}${alpha} \
+  --out_filename ${out_filename}_alpha${alpha} \
   --warm_start True \
-  --init_fit ${out_folder}${out_filename}6.0.npz \
-  --k_approx 30
+  --init_fit ${out_folder}${out_filename}_alpha6.0.npz \
+  --k_approx 25
