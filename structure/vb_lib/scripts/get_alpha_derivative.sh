60d7b30e16c68a88cb0ade78bd4ee8d20ea20ec2
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2020-11-22 14:30:41 -0800

    try alpha derivative on hgdp

--- structure/vb_lib/scripts/get_alpha_derivative.sh
+++ structure/vb_lib/scripts/get_alpha_derivative.sh
@@ -8,8 +8,11 @@ alpha_vec=(1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0 \
 alpha=${alpha_vec[$SLURM_ARRAY_TASK_ID]}
 echo $alpha 
 
-data_file=../simulated_data/simulated_structure_data_nobs20_nloci50_npop4.npz
-out_filename=simulated_fit
+# data_file=../simulated_data/simulated_structure_data_nobs20_nloci50_npop4.npz
+# out_filename=simulated_fit
+
+data_file=/accounts/grad/runjing_liu/BNP/fastStructure/hgdp_data/huang2011_plink_files/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz
+out_filename=huang2011_fit
 
 out_folder=../fits/fits_20201122/
 
