6d87f9733f5d7fc65e8a6bb393a362e4d9655691
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2020-11-30 13:30:48 -0800

    notebooks

--- structure/vb_lib/scripts/get_alpha_derivative.sh
+++ structure/vb_lib/scripts/get_alpha_derivative.sh
@@ -5,7 +5,8 @@ source activate bnp_sensitivity_jax
 alpha_vec=(1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0 \
             6.5 7.0 7.5 8.0 8.5 9.0 9.5 10.0 10.5 11.0)
             
-alpha=${alpha_vec[$SLURM_ARRAY_TASK_ID]}
+# alpha=${alpha_vec[$SLURM_ARRAY_TASK_ID]}
+alpha=6.0
 echo $alpha 
 
 # data_file=../simulated_data/simulated_structure_data_nobs20_nloci50_npop4.npz
