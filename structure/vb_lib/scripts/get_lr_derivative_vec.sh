80e7984a10900aeb31cb0d3a34e1f00eae415abf
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2020-12-08 11:03:26 -0800

    get lr derivative at other alphas

--- structure/vb_lib/scripts/get_lr_derivative_vec.sh
+++ structure/vb_lib/scripts/get_lr_derivative_vec.sh
@@ -5,7 +5,9 @@
 # (makes it easier to track nodes and resources)
 
 
-sbatch --export=job_indx=1 get_lr_derivative.sh
+# sbatch --export=job_indx=1 get_lr_derivative.sh
+sbatch --export=job_indx=0 get_lr_derivative.sh
+sbatch --export=job_indx=2 get_lr_derivative.sh
 
 # for i in {0..2}
 # do
