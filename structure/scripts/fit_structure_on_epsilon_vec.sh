74c3e6a3cf57444f9e8db9c91fc1e0397e50b8ff
Runjing Liu <runjing_liu@legolas.Berkeley.EDU>
2021-01-18 12:11:51 -0800

    notebook

--- structure/scripts/fit_structure_on_epsilon_vec.sh
+++ structure/scripts/fit_structure_on_epsilon_vec.sh
@@ -1,7 +1,7 @@
-# sbatch --array 0-10 --export=perturbation='worst_case',delta=1 fit_structure_on_epsilon.sh
+sbatch --array 0-18 --export=perturbation='worst_case',delta=1 fit_structure_on_epsilon.sh
 
-# sbatch --array 0-20 --export=perturbation='sigmoidal',delta=5 fit_structure_on_epsilon.sh
-sbatch --array 0-20 --export=perturbation='sigmoidal_neg',delta=5 fit_structure_on_epsilon.sh
+# sbatch --array 0-18 --export=perturbation='sigmoidal',delta=5 fit_structure_on_epsilon.sh
+# sbatch --array 0-18 --export=perturbation='sigmoidal_neg',delta=5 fit_structure_on_epsilon.sh
 
-# sbatch --array 0-25 --export=perturbation='alpha_pert_pos',delta=1 fit_structure_on_epsilon.sh
-# sbatch --array 0-25 --export=perturbation='alpha_pert_neg',delta=1 fit_structure_on_epsilon.sh
+# sbatch --array 0-18 --export=perturbation='alpha_pert_pos',delta=1 fit_structure_on_epsilon.sh
+# sbatch --array 0-18 --export=perturbation='alpha_pert_neg',delta=1 fit_structure_on_epsilon.sh
