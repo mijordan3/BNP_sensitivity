eb0c48643703d777e3b5e530a58c07a5275d6c9e
Runjing Liu <runjing_liu@legolas.Berkeley.EDU>
2021-01-18 14:43:39 -0800

    run on subset

--- structure/scripts/fit_structure_on_epsilon.sh
+++ structure/scripts/fit_structure_on_epsilon.sh
@@ -4,17 +4,13 @@ source activate bnp_sensitivity_jax
 
 epsilon_indx=$SLURM_ARRAY_TASK_ID
 
-data_file=../data/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz 
-out_filename=huang2011_fit
-out_folder=../fits/hgdp_fits/
+# data_file=../data/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz 
+# out_filename=huang2011_fit
+# out_folder=../fits/hgdp_fits/
 
-# data_file=../simulated_data/simulated_structure_data_nobs20_nloci50_npop4.npz
-# out_filename=testing
-# out_folder=../fits/tmp/
-
-# data_file=../data/huang2011_sub_nobs25_nloci75.npz
-# out_filename=huang2011_fit_sub
-# out_folder=../fits/tmp/
+data_file=../data/huang2011_sub_nobs25_nloci75.npz
+out_filename=huang2011_fit_sub
+out_folder=../fits/tmp/
 
 python fit_structure_perturbed.py \
   --perturbation ${perturbation} \
