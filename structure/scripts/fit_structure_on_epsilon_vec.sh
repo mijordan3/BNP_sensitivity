b2a1c45ebfee94ee9e92c10b02d6b7e601a5ca72
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2021-01-25 18:02:37 -0800

    more posterior statistics in expeirmental notebook

--- structure/scripts/fit_structure_on_epsilon_vec.sh
+++ structure/scripts/fit_structure_on_epsilon_vec.sh
@@ -10,7 +10,7 @@
 # sbatch --array 0-18 --export=perturbation='alpha_pert_neg_xflip',delta=1 fit_structure_on_epsilon.sh
 
 sbatch --array 0-18 --export=perturbation='gauss_pert1_pos',delta=1 fit_structure_on_epsilon.sh
-sbatch --array 0-18 --export=perturbation='gauss_pert1_neg',delta=1 fit_structure_on_epsilon.sh
+# sbatch --array 0-18 --export=perturbation='gauss_pert1_neg',delta=1 fit_structure_on_epsilon.sh
 
-sbatch --array 0-18 --export=perturbation='gauss_pert2_pos',delta=1 fit_structure_on_epsilon.sh
-sbatch --array 0-18 --export=perturbation='gauss_pert2_neg',delta=1 fit_structure_on_epsilon.sh
+# sbatch --array 0-18 --export=perturbation='gauss_pert2_pos',delta=1 fit_structure_on_epsilon.sh
+# sbatch --array 0-18 --export=perturbation='gauss_pert2_neg',delta=1 fit_structure_on_epsilon.sh
