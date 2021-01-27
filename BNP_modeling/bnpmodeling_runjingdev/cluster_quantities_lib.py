eb456fb12d3a558be88cc8c4a37ae2e02b047e81
Runjing Liu <runjing_liu@gimli.Berkeley.EDU>
2021-01-27 12:01:14 -0800

    fixes in cluster quantities lib and tests

--- BNP_modeling/bnpmodeling_runjingdev/cluster_quantities_lib.py
+++ BNP_modeling/bnpmodeling_runjingdev/cluster_quantities_lib.py
@@ -10,8 +10,7 @@ import bnpmodeling_runjingdev.exponential_families as ef
 
 def get_mixture_weights_from_stick_break_propns(stick_break_propns):
     """
-    Computes stick lengths (i.e. mixture weights) from stick breaking
-    proportions.
+    Computes mixture weights from stick breaking proportions.
 
     Parameters
     ----------
@@ -48,49 +47,37 @@ def get_mixture_weights_from_stick_break_propns(stick_break_propns):
 
 def get_e_cluster_probabilities(stick_propn_mean, stick_propn_info,
                                         gh_loc, gh_weights):
-    e_stick_lengths = \
-        ef.get_e_logitnormal(stick_propn_mean, stick_propn_info, gh_loc, gh_weights)
-
-    return get_mixture_weights_from_stick_break_propns(e_stick_lengths)
-
-def get_stick_break_propns_from_mixture_weights(mixture_weights):
     """
-    Computes stick breaking proportions from the mixture weights. Used
-    for initializing vb parameters
+    Computes the expected number of cluster weights from logit-normal 
+    parameters. 
 
     Parameters
     ----------
-    stick_break_propns : ndarray
-        Array of stick breaking proportions, with sticks along last dimension
-
+    stick_propn_mean : ndarray
+        Mean parameters for the logit of the
+        stick-breaking proportions, of shape ...  x (k_approx-1)
+    stick_propn_info : ndarray
+        parameters for the logit of the
+        stick-breaking proportions, of shape ...  x (k_approx-1)
+    
     Returns
     -------
-    stick_break_propns : ndarray
-        Array of stick breaking proportions, with sticks along last dimension
+    ndarray
+        array of mixture weights, of shape ... x k_approx
     """
+    
+    e_stick_lengths = \
+        ef.get_e_logitnormal(stick_propn_mean, stick_propn_info, gh_loc, gh_weights)
+    
+    # can just multiply expectations bc variational distribution on sticks 
+    # are independent 
+    return get_mixture_weights_from_stick_break_propns(e_stick_lengths)
 
-    # TODO: implement for more general array shapes
-    if not len(np.shape(mixture_weights)) == 2:
-        raise NotImplementedError
-
-    n_obs = mixture_weights.shape[0]
-    k_approx = mixture_weights.shape[1]
-
-    stick_break_propn = np.zeros((n_obs, k_approx - 1))
-    stick_remain = np.ones(n_obs)
-
-    for i in range(k_approx - 1):
-        stick_break_propn = jax.ops.index_update(stick_break_propn,
-                                                jax.ops.index[:, i],
-                                                mixture_weights[:, i] / stick_remain)
-
-        stick_remain *= (1 - stick_break_propn[:, i])
-
-    return stick_break_propn
 
 
 def get_e_number_clusters_from_logit_sticks(stick_propn_mean, stick_propn_info,
-                                            n_obs, threshold = 0,
+                                            n_obs, 
+                                            threshold = 0,
                                             n_samples = None,
                                             seed = 0,
                                             unv_norm_samples = None):
@@ -104,10 +91,10 @@ def get_e_number_clusters_from_logit_sticks(stick_propn_mean, stick_propn_info,
     ----------
     stick_propn_mean : ndarray
         Mean parameters for the logit of the
-        stick-breaking proportions, of shape ...  x k_approx
+        stick-breaking proportions, of length (k_approx - 1)
     stick_propn_info : ndarray
         parameters for the logit of the
-        stick-breaking proportions, of shape ...  x k_approx
+        stick-breaking proportions, of length (k_approx - 1)
     threshold : int
         Miniumum number of observations for a cluster to be counted.
     n_obs : int
@@ -128,19 +115,21 @@ def get_e_number_clusters_from_logit_sticks(stick_propn_mean, stick_propn_info,
     """
 
     assert stick_propn_mean.shape == stick_propn_info.shape
-    if len(stick_propn_mean.shape) == 1:
-        stick_propn_mean = stick_propn_mean[None, :]
-        stick_propn_info = stick_propn_info[None, :]
+    assert len(stick_propn_mean.shape) == 1
+    k_approx = len(stick_propn_mean)
+    
+    stick_propn_mean = stick_propn_mean[None, :]
+    stick_propn_info = stick_propn_info[None, :]
 
     assert (n_samples is not None) or (unv_norm_samples is not None), \
         'both n_samples and unv_norm_samples cannot be None'
 
     if unv_norm_samples is None:
-        unif_samples_shape = (n_samples, ) + stick_propn_mean.shape
+        unif_samples_shape = (n_samples, k_approx - 1)
         unv_norm_samples = random.normal(key = jax.random.PRNGKey(seed), 
                                          shape = unif_samples_shape)
     if n_samples is None:
-        assert unv_norm_samples.shape[1:] == stick_propn_mean.shape
+        assert unv_norm_samples.shape[1] == (k_approx - 1)
 
     # sample sticks proportions from logitnormal
     # this is n_samples x ... x (k_approx - 1)
@@ -148,8 +137,8 @@ def get_e_number_clusters_from_logit_sticks(stick_propn_mean, stick_propn_info,
                             1 / np.sqrt(stick_propn_info) + \
                                         stick_propn_mean)
 
-    # get posterior weights
-    # this is n_samples x ... x (k_approx - 1)
+    # get sampled posterior weights
+    # this is n_samples x _approx
     weight_samples = \
         get_mixture_weights_from_stick_break_propns(stick_propn_samples)
 
@@ -181,70 +170,27 @@ def get_e_num_clusters_from_ez(e_z):
     k = np.shape(e_z)[1]
     return k - np.sum(np.prod(1 - e_z, axis = 0))
 
-def get_e_num_clusters_from_ez_2d(e_z): 
-    # e_z is of shape n_samples x ... x k_approx
-    # returns the expected number of clusters 
-    # for each n_sample (output is of length n_samples)
-    
-    n_samples = e_z.shape[0]
-    k_approx = e_z.shape[-1]
-    
-    e_z = e_z.reshape(n_samples, -1, k_approx)
-    
-    return k_approx - (1 - e_z).prod(1).sum(-1)    
-
-def sample_ez(e_z, n_samples, seed, return_one_hot = True): 
-    # e_z is of shape n x k
-    # index (n,k) is probability of n-th observation 
-    # belonging to cluster k
-    
-    logits = np.log(e_z)
-    
-    z_samples = random.categorical(key = random.PRNGKey(seed), 
-                               logits = logits, 
-                               shape = (n_samples, e_z.shape[0]))
-    
-    if return_one_hot: 
-        # shape is n_samples x n x k
-        return jax.nn.one_hot(z_samples, num_classes = e_z.shape[1])
-    else: 
-        return z_samples
 
 
-###################
-# TODO: combine the two functions below 
-###################
-def _get_clusters_from_ez_and_unif_samples(e_z_cumsum, unif_samples):
-    # returns a n_obs x n_samples binary  matrix encoding the cluster belonging
-    # of the nth observation in nth sample
-
-    n_obs = e_z_cumsum.shape[0]
-
-    # unif_sample should be a matrix of shape n_obs x n_samples
-    assert len(unif_samples.shape) == 2
-    assert unif_samples.shape[0] == n_obs
-
-    # get which cluster the sample belongs to
-    z_sample = (e_z_cumsum[:, :, None] > unif_samples[:, None, :]).argmax(1)
-
-    return z_sample
-
-def _get_onehot_clusters_from_ez_and_unif_samples(e_z, unif_samples):
+def _sample_ez_from_unif_samples(e_z, unif_samples):
     # e_z is n_obs x k
     # unif_sample should be a matrix of shape n_samples x n_obs
     
     # returns a n_samples x n_obs x k matrix encoding sampled 
     # cluster belongings
     
+    assert np.all(e_z >= 0.)
+    assert np.all(e_z <= 1.)
+    
     n_obs = e_z.shape[0]
     k_approx = e_z.shape[1]
     
     e_z_cumsum = e_z.cumsum(1)
+    assert np.all(np.abs(e_z_cumsum[:, -1] - 1.) < 1e-8)
+        
     e_z_cumsum0 = np.hstack((np.zeros((n_obs, 1)),
                              e_z_cumsum[:, 0:(k_approx-1)]))
     
-    n_obs = e_z_cumsum.shape[0]
-
     assert len(unif_samples.shape) == 2
     assert unif_samples.shape[1] == n_obs
 
@@ -254,9 +200,34 @@ def _get_onehot_clusters_from_ez_and_unif_samples(e_z, unif_samples):
 
     return z_sample
 
+def sample_ez(e_z, 
+              n_samples = 1, 
+              seed = 0, 
+              unif_samples = None): 
+ 
+    # e_z is of shape n x k
+    # index (n,k) is probability of n-th observation 
+    # belonging to cluster k
+    
+    n_obs = e_z.shape[0]
+    
+    # draw uniform samples
+    if unif_samples is None:
+        unif_samples = random.uniform(key = random.PRNGKey(seed), 
+                                      shape = (n_samples, n_obs))
+
+    else:
+        assert unif_samples.shape[1] == n_obs
+    
+    # one-hot encoding of zs
+    z_samples_one_hot = _sample_ez_from_unif_samples(e_z, unif_samples)
+    
+    # shape is n_samples x n x k
+    return z_samples_one_hot
+    
 def get_e_num_large_clusters_from_ez(e_z,
                                     threshold = 0,
-                                    n_samples = None,
+                                    n_samples = 1,
                                     seed = 0,
                                     unif_samples = None):
     """
@@ -283,29 +254,19 @@ def get_e_num_large_clusters_from_ez(e_z,
         in a dataset the same size as e_z
     """
 
-    n_obs = e_z.shape[0]
-    n_clusters = e_z.shape[1]
+    # z_sample is a n_samples x n_obs x k_approx matrix of cluster belongings
+    z_sample = sample_ez(e_z, 
+                         n_samples = n_samples, 
+                         seed = seed,
+                         unif_samples = unif_samples)
+    
+    # this is n_samples x k_approx: 
+    # for each sample, the number of individuals in each cluster
+    counts_sampled = z_sample.sum(1)
+    
+    # for each sample, the number of clusters above some threshold
+    n_clusters_sampled = (counts_sampled > threshold).sum(1)
+    
+    return np.mean(n_clusters_sampled), np.var(n_clusters_sampled)
 
-    # draw uniform samples
-    if unif_samples is None:
-        assert n_samples is not None
-        unif_samples = random.uniform(key = random.PRNGKey(seed), 
-                                      shape = (n_obs, n_samples))
 
-    else:
-        assert unif_samples is not None
-        assert unif_samples.shape[0] == n_obs
-
-    n_samples = unif_samples.shape[1]
-    e_z_cumsum = np.cumsum(e_z, axis = 1)
-
-    num_heavy_clusters_vec = np.zeros(n_samples)
-
-    # z_sample is a n_obs x n_samples matrix of cluster belongings
-    z_sample = _get_clusters_from_ez_and_unif_samples(e_z_cumsum, unif_samples)
-
-    for i in range(n_clusters):
-        # get number of clusters with at least enough points above the threshold
-        num_heavy_clusters_vec += np.sum(z_sample == i, axis = 0) > threshold
-
-    return np.mean(num_heavy_clusters_vec), np.var(num_heavy_clusters_vec)
