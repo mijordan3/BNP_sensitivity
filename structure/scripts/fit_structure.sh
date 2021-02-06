e0ab9fa00ae337ef7b18df3aa8443bfc52567b42
Runjing Liu <runjing_liu@legolas.Berkeley.EDU>
2021-02-06 11:31:44 -0800

    revert back

--- structure/scripts/fit_structure.sh
+++ structure/scripts/fit_structure.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-echo 'warning! using bnp_sensitivity_testing env'
-source activate bnp_sensitivity_testing
+source activate bnp_sensitivity_jax
 
 alpha=6.0 
 
 seed=45319801
 
 data_file=../data/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz 
-out_filename=testing # huang2011_fit
+out_filename=huang2011_fit
 out_folder=../fits/hgdp_fits/
 
 # data_file=../simulated_data/simulated_structure_data_nobs1107_nloci2810_npop6.npz
