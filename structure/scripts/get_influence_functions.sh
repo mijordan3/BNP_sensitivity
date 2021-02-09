eb7d3fe6f4bd2082478c9fe9f80d5a98de285a05
Runjing Liu <runjing_liu@legolas.Berkeley.EDU>
2021-02-08 20:35:44 -0800

    influence function on hgdp data crashes

--- structure/scripts/get_influence_functions.sh
+++ structure/scripts/get_influence_functions.sh
@@ -4,13 +4,13 @@ source activate bnp_sensitivity_jax
 
 alpha=6.0
 
-# data_file=../data/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz 
-# out_filename=huang2011_fit
-# out_folder=../fits/hgdp_fits/
+data_file=../data/phased_HGDP+India+Africa_2810SNPs-regions1to36.npz 
+out_filename=huang2011_fit
+out_folder=../fits/hgdp_fits/
 
-data_file=../data/huang2011_sub_nobs25_nloci75.npz
-out_filename=huang2011_fit_sub
-out_folder=../fits/tmp/
+# data_file=../data/huang2011_sub_nobs25_nloci75.npz
+# out_filename=huang2011_fit_sub
+# out_folder=../fits/tmp/
 
 python get_influence_functions.py \
   --data_file ${data_file} \
