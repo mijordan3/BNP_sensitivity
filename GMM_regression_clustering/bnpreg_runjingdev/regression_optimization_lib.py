6e06c12bc653853bb93b7d15af86faedb0d64718
Runjing Liu <runjing_liu@legolas.Berkeley.EDU>
2021-02-20 11:38:07 -0800

    weird optimum needs debugging

---
+++ GMM_regression_clustering/bnpreg_runjingdev/regression_optimization_lib.py
@@ -0,0 +1,89 @@
+import jax 
+import jax.numpy as np
+
+import numpy as onp
+
+import time
+
+from bnpmodeling_runjingdev.sensitivity_lib import get_jac_hvp_fun
+
+from bnpreg_runjingdev import regression_mixture_lib 
+from bnpreg_runjingdev.regression_posterior_quantities import get_optimal_z_from_vb_dict
+
+from scipy.optimize import minimize
+        
+def optimize_regression_mixture(gamma, gamma_info, 
+                                vb_params_dict, 
+                                vb_params_paragami,
+                                prior_params_dict, 
+                                gh_loc, gh_weights, 
+                                e_log_phi = None): 
+    
+    ###################
+    # Define loss
+    ###################
+    def _get_loss(vb_params_free): 
+        
+        vb_params_dict = vb_params_paragami.fold(vb_params_free, free = True)
+    
+        return regression_mixture_lib.get_kl(gamma, 
+                                             gamma_info,
+                                             vb_params_dict,
+                                             prior_params_dict,
+                                             gh_loc,
+                                             gh_weights)
+    
+    get_loss = jax.jit(_get_loss)
+    get_grad = jax.jit(jax.grad(_get_loss))
+    get_hvp = jax.jit(get_jac_hvp_fun(_get_loss))
+    
+    
+    ################
+    # compile objective functions
+    ################
+    # intial point
+    x0 = vb_params_paragami.flatten(vb_params_dict, free = True)
+    
+    print('compiling objective and derivatives ... ')
+    t0 = time.time()
+    _ = get_loss(x0).block_until_ready()
+    _ = get_grad(x0).block_until_ready()
+    _ = get_hvp(x0, x0).block_until_ready()
+    print('done. Compile time: {0:.03f}sec'.format(time.time() - t0))
+    
+    ################
+    # initialize with L-BFGS-B
+    ################
+    print('Running L-BFGS-B ...')
+    t0 = time.time() 
+    lbfgs_out = minimize(fun = lambda x : onp.array(get_loss(x)), 
+                         x0 = x0, 
+                         method = 'L-BFGS-B', 
+                         jac = lambda x : onp.array(get_grad(x)))    
+    print('L-BFGS-B time: {:.03f}sec'.format(time.time() - t0))
+    
+    ################
+    # run a few more newton steps
+    ################
+    t1 = time.time() 
+    print('Running trust-ncg ... ')
+    out = minimize(fun = lambda x : onp.array(get_loss(x)), 
+               x0 = lbfgs_out.x, 
+               method = 'trust-ncg', 
+               jac = lambda x : onp.array(get_grad(x)), 
+               hessp = lambda x,v : onp.array(get_hvp(x, v)))    
+    
+    vb_opt = out.x
+    vb_opt_dict = vb_params_paragami.fold(vb_opt, free = True)
+    print(out.message)
+    print('Newton time: {:.03f}sec'.format(time.time() - t1))
+    print('done. ')
+    
+    optim_time = time.time() - t0
+    
+    # compute optimal ez
+    ez_opt = get_optimal_z_from_vb_dict(gamma, gamma_info,
+                                        vb_opt_dict,
+                                        gh_loc, gh_weights)
+    
+    return vb_opt_dict, vb_opt, ez_opt, out, optim_time
---
+++ GMM_regression_clustering/bnpreg_runjingdev/regression_posterior_quantities.py
@@ -0,0 +1,107 @@
+# some function to compute 
+# posterior quantities of interest
+
+import jax 
+import jax.numpy as np
+
+from bnpreg_runjingdev import regression_mixture_lib
+
+from bnpgmm_runjingdev.gmm_clustering_lib import get_e_mixture_weights_from_vb_dict
+
+def get_optimal_z_from_vb_dict(gamma, gamma_info, vb_params_dict,
+                               gh_loc, gh_weights,
+                               use_bnp_prior = True):
+
+    """
+    Returns the optimal cluster belonging probabilities, given the
+    variational parameters.
+
+    Parameters
+    ----------
+    y : ndarray
+        The array of datapoints, one observation per row.
+    vb_params_dict : dictionary
+        Dictionary of variational parameters.
+    gh_loc : vector
+        Locations for gauss-hermite quadrature. We need this compute the
+        expected prior terms.
+    gh_weights : vector
+        Weights for gauss-hermite quadrature. We need this compute the
+        expected prior terms.
+    use_bnp_prior : boolean
+        Whether or not to use a prior on the cluster mixture weights.
+        If True, a DP prior is used.
+
+    Returns
+    -------
+    e_z : ndarray
+        The optimal cluster belongings as a function of the variational
+        parameters, stored in an array whose (n, k)th entry is the probability
+        of the nth datapoint belonging to cluster k
+
+    """
+
+    # get global vb parameters
+    stick_means = vb_params_dict['stick_params']['stick_means']
+    stick_infos = vb_params_dict['stick_params']['stick_infos']
+    centroids = vb_params_dict['centroids']
+    
+    # compute optimal e_z from vb global parameters
+    e_z, _ = regression_mixture_lib.get_optimal_z(gamma, gamma_info, 
+                                                  stick_means, stick_infos,
+                                                  centroids,
+                                                  gh_loc, gh_weights,
+                                                  use_bnp_prior = use_bnp_prior)
+
+    return e_z
+
+# def get_e_mixture_weights_from_vb_dict(vb_params_dict, gh_loc, gh_weights): 
+#     stick_means = vb_params_dict['stick_params']['stick_means']
+#     stick_infos = vb_params_dict['stick_params']['stick_infos']
+    
+#     weights = cluster_lib.get_e_cluster_probabilities(stick_means, 
+#                                                       stick_infos,
+#                                                       gh_loc,
+#                                                       gh_weights)
+    
+#     return weights
+
+
+
+# def get_e_num_pred_clusters_from_vb_dict(vb_params_dict,
+#                                          n_obs,
+#                                          threshold = 0,
+#                                          n_samples = 10000,
+#                                          prng_key = jax.random.PRNGKey(0)):
+    
+#     # get posterior predicted number of clusters
+
+#     stick_means = vb_params_dict['stick_params']['stick_means']
+#     stick_infos = vb_params_dict['stick_params']['stick_infos']
+
+#     return cluster_lib.get_e_num_pred_clusters_from_logit_sticks(stick_means,
+#                                                                  stick_infos,
+#                                                                  n_obs,
+#                                                                  threshold = threshold,
+#                                                                  n_samples = n_samples,
+#                                                                  prng_key = prng_key)
+
+
+# # Get the expected posterior number of distinct clusters.
+# def get_e_num_clusters_from_vb_dict(y, 
+#                                     vb_params_dict,
+#                                     gh_loc, gh_weights,
+#                                     threshold = 0,
+#                                     n_samples = 10000,
+#                                     prng_key = jax.random.PRNGKey(0)):
+
+#     e_z  = get_optimal_z_from_vb_dict(y, 
+#                                       vb_params_dict,
+#                                       gh_loc,
+#                                       gh_weights,
+#                                       use_bnp_prior = True)
+
+#     return cluster_lib.get_e_num_clusters_from_ez(e_z,
+#                                                   threshold = threshold,
+#                                                   n_samples = n_samples,
+#                                                   prng_key = prng_key)
