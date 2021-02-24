b2ad6321e982ad3a1a718f336c7e465496e164a0
Runjing Liu <runjing_liu@legolas.Berkeley.EDU>
2021-02-24 14:52:11 -0800

    test for both optimal shifts and zs

--- GMM_regression_clustering/bnpreg_runjingdev/tests/test_regression_mixture.py
+++ GMM_regression_clustering/bnpreg_runjingdev/tests/test_regression_mixture.py
@@ -56,6 +56,7 @@ def get_local_params_paragami():
     return local_params_dict, local_params_paragami
 
 class TestRegressionMixture(unittest.TestCase):
+    
     def test_optimal_shifts(self): 
         
         # get paragami object for local parameters
@@ -108,6 +109,74 @@ class TestRegressionMixture(unittest.TestCase):
         shift_grad = jax.grad(get_shift_loss)(local_params_free)
         
         assert np.abs(shift_grad).max() < 1e-6
+    
+    def test_optimal_local_params(self): 
         
+                
+        # get paragami object for local parameters
+        local_params_dict, local_params_paragami = \
+            get_local_params_paragami()
+        
+        # set optimal shifts and z's
+        _, ez_free, e_b, e_b2 = \
+            get_optimal_local_params_from_vb_dict(y, regressors, vb_params_dict, prior_params_dict, 
+                                                  gh_loc, gh_weights)
+        
+        
+        local_params_dict['e_b'] = e_b
+        local_params_dict['e_b2'] = e_b2
+        local_params_dict['ez_free'] = ez_free
+
+        # kl as function of local parameters
+        def get_local_kl(local_free_params): 
+    
+            # get local parameters
+            local_params_dict = local_params_paragami.fold(local_free_params, free = True)
+
+            e_b = local_params_dict['e_b']
+            e_b2 = local_params_dict['e_b2']
+            ez_free = local_params_dict['ez_free']
+
+            e_z = jax.nn.softmax(ez_free, axis = 1)
+
+
+            # get other (aka global) vb parameters
+            stick_means = vb_params_dict['stick_params']['stick_means']
+            stick_infos = vb_params_dict['stick_params']['stick_infos']
+            centroids = vb_params_dict['centroids']
+            data_info = vb_params_dict['data_info']
+
+            z_nat_param = \
+                    regression_mixture_lib.get_optimal_z(y, regressors, 
+                                  stick_means, stick_infos,
+                                  data_info, centroids,
+                                  e_b, e_b2, 
+                                  gh_loc, gh_weights, 
+                                  prior_params_dict)[1]
+
+            e_loglik = np.sum(e_z * z_nat_param) 
+
+            # entropy term
+            entropy = regression_mixture_lib.get_entropy(stick_means, stick_infos, e_z,
+                                                         e_b, e_b2, 
+                                                         gh_loc, gh_weights)
+
+            # prior term
+            e_log_prior = regression_mixture_lib.get_e_log_prior(stick_means, stick_infos, 
+                                            data_info, centroids,
+                                            prior_params_dict,
+                                            gh_loc, gh_weights)
+
+            elbo = e_log_prior + entropy + e_loglik
+
+            return -1 * elbo.squeeze()
+    
+        
+        local_params_free = local_params_paragami.flatten(local_params_dict, free = True)
+        local_grad = jax.grad(get_local_kl)(local_params_free)
+
+        assert np.abs(local_grad).max() < 1e-6
+    
+    
 if __name__ == '__main__':
     unittest.main()
