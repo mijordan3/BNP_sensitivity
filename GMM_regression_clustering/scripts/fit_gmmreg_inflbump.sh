bf38322b2959c42da942fd551eee2ed9fd98a060
Runjing Liu <runjing_liu@legolas.Berkeley.EDU>
2021-03-04 21:20:37 -0800

    influence bumps

---
+++ GMM_regression_clustering/scripts/fit_gmmreg_inflbump.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source activate bnp_sensitivity_jax
+
+epsilon_indx=$SLURM_ARRAY_TASK_ID
+
+python fit_gmmreg_inflbump.py \
+  --epsilon_indx ${epsilon_indx} \
+  --mu_indx ${mu_indx} \
+  --out_folder ${out_folder} \
+  --out_filename ${out_filename}_inflbump_mu${mu_indx}_eps${epsilon_indx} \
+  --init_fit ${out_folder}${out_filename}_alpha6.0.npz 
--- GMM_regression_clustering/scripts/run_mice_data_experiments.sh
+++ GMM_regression_clustering/scripts/run_mice_data_experiments.sh
@@ -6,62 +6,75 @@ alpha=6.0
 out_folder='../fits/'
 out_filename='mice_genomics_fit'
 
+# #################
+# # Initial fit
+# #################
+# python fit_gmm_regression.py \
+#   --seed ${seed} \
+#   --alpha ${alpha} \
+#   --out_folder ${out_folder} \
+#   --out_filename ${out_filename}_alpha${alpha} \
+
+
+# #################
+# # compute derivative
+# #################
+# python get_lr_derivative.py \
+#   --out_folder ${out_folder} \
+#   --fit_file ${out_filename}_alpha${alpha}.npz \
+
+# #################
+# # Refits
+# #################
+
+# # sigmoidal perturbations
+# sbatch \
+#     --array 0-18 \
+#     --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='sigmoidal',delta=5 \
+#     fit_gmmreg_perturbed.sh
+
+# sbatch \
+#     --array 0-18 \
+#     --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='sigmoidal',delta=-5 \
+#     fit_gmmreg_perturbed.sh
+
+# # alpha perturbation
+# sbatch \
+#     --array 0-18 \
+#     --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='alpha_pert_pos',delta=1 \
+#     fit_gmmreg_perturbed.sh
+# sbatch \
+#     --array 0-18 \
+#     --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='alpha_pert_neg',delta=1 \
+#     fit_gmmreg_perturbed.sh
+
+# # gaussian bumps
+# sbatch \
+#     --array 0-18 \
+#     --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='gauss_pert1',delta=2 \
+#     fit_gmmreg_perturbed.sh
+# sbatch \
+#     --array 0-18 \
+#     --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='gauss_pert1',delta=-2 \
+#     fit_gmmreg_perturbed.sh
+# sbatch \
+#     --array 0-18 \
+#     --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='gauss_pert2',delta=2 \
+#     fit_gmmreg_perturbed.sh
+# sbatch \
+#     --array 0-18 \
+#     --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='gauss_pert2',delta=-2 \
+#     fit_gmmreg_perturbed.sh
+
 #################
-# Initial fit
+# compute influence functions
 #################
-python fit_gmm_regression.py \
-  --seed ${seed} \
-  --alpha ${alpha} \
-  --out_folder ${out_folder} \
-  --out_filename ${out_filename}_alpha${alpha} \
+# python get_influence_functions.py \
+#   --out_folder ${out_folder} \
+#   --fit_file ${out_filename}_alpha${alpha}.npz \
 
-
-#################
-# compute derivative
-#################
-python get_lr_derivative.py \
-  --out_folder ${out_folder} \
-  --fit_file ${out_filename}_alpha${alpha}.npz \
-
-#################
-# Refits
-#################
-
-# sigmoidal perturbations
-sbatch \
-    --array 0-18 \
-    --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='sigmoidal',delta=5 \
-    fit_gmmreg_perturbed.sh
-
-sbatch \
-    --array 0-18 \
-    --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='sigmoidal',delta=-5 \
-    fit_gmmreg_perturbed.sh
-
-# alpha perturbation
-sbatch \
-    --array 0-18 \
-    --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='alpha_pert_pos',delta=1 \
-    fit_gmmreg_perturbed.sh
-sbatch \
-    --array 0-18 \
-    --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='alpha_pert_neg',delta=1 \
-    fit_gmmreg_perturbed.sh
-
-# gaussian bumps
-sbatch \
-    --array 0-18 \
-    --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='gauss_pert1',delta=2 \
-    fit_gmmreg_perturbed.sh
-sbatch \
-    --array 0-18 \
-    --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='gauss_pert1',delta=-2 \
-    fit_gmmreg_perturbed.sh
-sbatch \
-    --array 0-18 \
-    --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='gauss_pert2',delta=2 \
-    fit_gmmreg_perturbed.sh
-sbatch \
-    --array 0-18 \
-    --export=alpha=$alpha,out_folder=$out_folder,out_filename=$out_filename,perturbation='gauss_pert2',delta=-2 \
-    fit_gmmreg_perturbed.sh
+# refit at step functions
+for mu_indx in {0..12}
+do
+   sbatch --array 0-8 --export=mu_indx=$mu_indx,out_folder=$out_folder,out_filename=$out_filename fit_gmmreg_inflbump.sh
+done
