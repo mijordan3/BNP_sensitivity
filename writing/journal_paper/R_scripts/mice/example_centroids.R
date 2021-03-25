af923fec4395e826c0664a5189ee254f881b8991
runjing_liu <runjing_liu@berkeley.edu>
2021-03-25 09:53:14 -0700

    change to style w better margins

--- writing/journal_paper/R_scripts/mice/example_centroids.R
+++ writing/journal_paper/R_scripts/mice/example_centroids.R
@@ -62,5 +62,5 @@ expression_data %>%
   theme(strip.background = element_blank(),
         strip.text.x = element_blank()) + 
   xlab('time (hours)') + 
-  ylab('expression (shifted)')
+  ylab('gene expr. (shifted)')
 
