41f189d6269927d87d542ec7a01f71b224f289b6
runjing_liu <runjing_liu@berkeley.edu>
2021-04-28 12:06:16 -0700

    knitr for result slides

--- writing/ISBA_slides_Apr2021/knit_figs.sh
+++ writing/ISBA_slides_Apr2021/knit_figs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-Rscript -e 'library(knitr); knit("figs.Rnw")'
+# Rscript -e 'library(knitr); knit("figs.Rnw")'
+
+Rscript -e 'library(knitr); knit("iris_figs.Rnw")'
+
+Rscript -e 'library(knitr); knit("structure_figs.Rnw")'
