788fc757532697483ffe30bd2b8c807966a7fd49
runjing_liu <runjing_liu@berkeley.edu>
2021-04-28 14:30:38 -0700

    separate scripts for knitr

--- writing/ISBA_slides_Apr2021/knit_figs.sh
+++ writing/ISBA_slides_Apr2021/knit_figs.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Rscript -e 'library(knitr); knit("figs.Rnw")'
-
-Rscript -e 'library(knitr); knit("iris_figs.Rnw")'
-
-Rscript -e 'library(knitr); knit("structure_figs.Rnw")'
+Rscript -e 'library(knitr); knit("figs.Rnw")'
