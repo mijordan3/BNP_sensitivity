7f7911f32812a7ce1b2cc0c2be06b3d37498092c
Ryan Giordano <rgiordan@gmail.com>
2021-04-29 08:55:19 -0700

    sync sim data

---
+++ writing/journal_paper/R_scripts/data_simulated/push_data_to_scf.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+DEST_PATH=/scratch/users/genomic_times_series_bnp/bnp_sensitivity_paper_simulated_data
+scp *.Rdata rgiordano@arwen.berkeley.edu:$DEST_PATH
---
+++ writing/journal_paper/R_scripts/sync_data_runjing.sh
@@ -0,0 +1,7 @@
+rsync -avz \
+   runjing_liu@arwen.berkeley.edu:/scratch/users/genomic_times_series_bnp/bnp_sensitivity_paper_data/. \
+   /home/rliu/Documents/BNP/BNP_sensitivity/writing/journal_paper/R_scripts/data_raw/.
+
+rsync -avz \
+  runjing_liu@arwen.berkeley.edu:/scratch/users/genomic_times_series_bnp/bnp_sensitivity_paper_simulated_data/. \
+  /home/rliu/Documents/BNP/BNP_sensitivity/writing/journal_paper/R_scripts/data_simulated/.
