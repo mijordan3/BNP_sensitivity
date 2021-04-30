377029f5031d2884ca8503fc50d928d952184c81
Ryan Giordano <rgiordan@gmail.com>
2021-04-30 11:41:07 -0700

    update figures

--- writing/journal_paper/R_scripts/debug.R
+++ writing/journal_paper/R_scripts/debug.R
@@ -3,6 +3,7 @@ paper_directory <- file.path(git_repo_loc, "writing/journal_paper")
 setwd(paper_directory)
 knitr_debug <- TRUE # Set to true to see error output
 simple_cache <- FALSE # Set to true to cache knitr output for this analysis.
+r_script_lib <- file.path(paper_directory, "R_scripts")
 source("R_scripts/initialize.R", echo=FALSE)
 source("R_scripts/plotting_utils.R")
 
@@ -13,6 +14,7 @@ source("R_scripts/plotting_utils.R")
 source('./R_scripts/simple_examples/load_data.R', echo=knitr_debug, print.eval=TRUE)
 
 #source('./R_scripts/simple_examples/function_paths.R', echo=knitr_debug, print.eval=TRUE)
-source('./R_scripts/simple_examples/function_paths_mult.R', echo=knitr_debug, print.eval=TRUE)
-source('./R_scripts/simple_examples/function_paths_lin.R', echo=knitr_debug, print.eval=TRUE)
+#source('./R_scripts/simple_examples/function_paths_mult.R', echo=knitr_debug, print.eval=TRUE)
+#source('./R_scripts/simple_examples/function_paths_lin.R', echo=knitr_debug, print.eval=TRUE)
 #source('./R_scripts/simple_examples/function_ball.R', echo=knitr_debug, print.eval=TRUE)
+source('./R_scripts/simple_examples/distant_distributions.R', echo=knitr_debug, print.eval=TRUE)
---
+++ writing/journal_paper/R_scripts/simple_examples/distant_distributions.R
@@ -0,0 +1,18 @@
+#
+# MakeDistPlot <- function(plot_var) {
+#   ggplot(df_distant, aes(x=theta)) +
+#       geom_line(aes(y=get(plot_var)), color="blue") +
+#       ylab("Density") + xlab("stick propn")
+# }
+#
+# grid.arrange(
+#   MakeDistPlot("p"),
+#   MakeDistPlot("p1"),
+#   ncol=2
+# )
+
+df_distant <- path_env$df_distant
+
+ggplot(df_distant, aes(x=theta)) +
+    geom_line(aes(y=0.96 * p), color="blue") +
+    geom_line(aes(y=p1), color="red") + ylab(NULL) + xlab("stick propn")
