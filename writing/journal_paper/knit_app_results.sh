8eec175e5630b1de95586cc603d653c79a4f1b4e
runjing_liu <runjing_liu@berkeley.edu>
2021-06-21 15:55:32 -0700

    copy over tamaras shared folder

---
+++ writing/journal_paper/knit_app_results.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./knit_app_results_iris.sh
+./knit_app_results_mice.sh
+./knit_app_results_structure.sh
---
+++ writing/journal_paper/knit_app_results_mice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+Rscript -e 'library(knitr); knit("app_results_mice.Rnw")'
