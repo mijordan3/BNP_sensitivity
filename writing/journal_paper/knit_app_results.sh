679fd2275ea9f195e7ff331b81cb51ac50bc2fba
runjing_liu <runjing_liu@berkeley.edu>
2021-06-22 13:37:33 -0700

    remove unnecessary appendix files

--- writing/journal_paper/knit_app_results.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./knit_app_results_iris.sh
-./knit_app_results_mice.sh
-./knit_app_results_structure.sh
--- writing/journal_paper/knit_app_results_mice.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-Rscript -e 'library(knitr); knit("app_results_mice.Rnw")'
